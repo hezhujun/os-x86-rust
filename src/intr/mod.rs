8096aaa21e72d461b26ce638a86a6abb75deaeaa
hezhujun <1214507717@qq.com>
2024-07-16 23:34:50 +0800

    以注册方式设置中断的处理程序

--- src/intr/mod.rs
+++ src/intr/mod.rs
@@ -1,6 +1,7 @@
 
 mod define;
 mod context;
+mod pic;
 
 pub use context::*;
 use core::arch::{asm, global_asm};
@@ -8,8 +9,7 @@ use alloc::sync::Arc;
 use define::*;
 use spin::Mutex;
 use IrqType::TIME;
-use crate::arch::x86::{outb, DescriptorTablePointer, GateDescriptor};
-use crate::arch::x86::pic::{self, OCW2};
+use crate::arch::x86::{DescriptorTablePointer, GateDescriptor};
 use crate::schedule::suspend_current_and_run_next;
 
 global_asm!(include_str!("trap.S"));
@@ -17,37 +17,10 @@ global_asm!(include_str!("trap.S"));
 const IDT_LEN: usize = 0x31;
 const IDT_MAX_LEN: usize = 256;
 
-/// 主片的控制端口
-const PIC_M_CTRL: u16 = 0x20;
-/// 主片的数据端口
-const PIC_M_DATA: u16 = 0x21;
-/// 主片的控制端口
-const PIC_S_CTRL: u16 = 0xA0;
-/// 主片的数据端口
-const PIC_S_DATA: u16 = 0xA1;
+type IntrHandlerFn = fn(IntrContext);
 
-fn pic_init() {
-    // 初始化主片
-    assert_eq!(pic::ICW1::new(true, false, false).0, 0x11u8);
-    outb(PIC_M_CTRL, pic::ICW1::new(true, false, false).0);  // ICW1: 边缘触发，级联 8259，需要ICW4
-    outb(PIC_M_DATA, pic::ICW2(0x20).0);  // ICW2: 起始中断向量为 0x20
-    assert_eq!(pic::ICW3::master(2).0, 0x04u8);
-    outb(PIC_M_DATA, pic::ICW3::master(2).0);  // ICW3: IR2 接从片
-    assert_eq!(pic::ICW4::uPM.bits(), 0x01u8);
-    outb(PIC_M_DATA, pic::ICW4::uPM.bits());  // ICW4: 8086 模式，正常 EOI
-
-    // 初始化从片
-    assert_eq!(pic::ICW1::new(true, false, false).0, 0x11u8);
-    outb(PIC_S_CTRL, pic::ICW1::new(true, false, false).0);  // ICW1: ICW1: 边缘触发，级联 8259，需要ICW4
-    outb(PIC_S_DATA, pic::ICW2(0x28).0);  // ICW2: 起始中断向量为 0x28
-    assert_eq!(pic::ICW3::slaver(2).0, 0x02u8);
-    outb(PIC_S_DATA, pic::ICW3::slaver(2).0);  // ICW3: 设置从片连接到主片的IR2引脚
-    assert_eq!(pic::ICW4::uPM.bits(), 0x01u8);
-    outb(PIC_S_DATA, pic::ICW4::uPM.bits());  // ICW4: 8086 模式，正常 EOI
-
-    // 只打开时钟中断
-    outb(PIC_M_DATA, 0xfe);
-    outb(PIC_S_DATA, 0xff);
+lazy_static! {
+    static ref INTR_HANDLER_TABLE: Arc<Mutex<[IntrHandlerFn; IDT_MAX_LEN]>> = Arc::new(Mutex::new([default_intr_handler; IDT_MAX_LEN]));
 }
 
 pub fn init() {
@@ -56,7 +29,7 @@ pub fn init() {
     }
 
     define::init();
-    pic_init();
+    pic::init();
 
     let idt_pointer = DescriptorTablePointer::new((intr_table as usize).try_into().unwrap(), (IDT_MAX_LEN * 8 - 1).try_into().unwrap());
     unsafe {
@@ -81,32 +54,19 @@ pub extern "C" fn intr_handler(mut intr_context: IntrContext) {
     let cs = intr_context.cs;
     // debug!("intr #{}({:#x}) error code {} {} eip {:#x} cs {:#x}", intr, intr, error_code, IrqErrorCode(error_code), eip, cs);
     assert!((intr >> 8) == 0);
-    let intr: u8 = (intr & 0xff).try_into().unwrap();
-    match intr {
-        IrqType::TIME => {
-            assert_eq!(pic::OCW2::new(false, false, true, 0).0, 0x20);
-            outb(PIC_M_CTRL, pic::OCW2::new(false, false, true, 0).0);
-
-            suspend_current_and_run_next();
-        },
-        IrqType::KEYBOARD | IrqType::IRQ_0X22 | IrqType::IRQ_0X23 | IrqType::IRQ_0X24 | IrqType::IRQ_0X25 | IrqType::IRQ_0X26 | IrqType::IRQ_0X27 => {
-            assert_eq!(pic::OCW2::new(false, false, true, 0).0, 0x20);
-            outb(PIC_M_CTRL, pic::OCW2::new(false, false, true, 0).0);
-        },
-        IrqType::IRQ_0X28 | IrqType::IRQ_0X29 | IrqType::IRQ_0X2A | IrqType::IRQ_0X2B | IrqType::IRQ_0X2C | IrqType::IRQ_0X2D | IrqType::IRQ_0X2E | IrqType::IRQ_0X2F => {
-            assert_eq!(pic::OCW2::new(false, false, true, 0).0, 0x20);
-            outb(PIC_S_CTRL, pic::OCW2::new(false, false, true, 0).0);
-            outb(PIC_M_CTRL, pic::OCW2::new(false, false, true, 0).0);
-        },
-        _ => {
-            info!("no handle intr");
-            loop {
-                
-            }
-        }
-    }
+    INTR_HANDLER_TABLE.lock()[intr](intr_context);
 }
 
 extern "C" {
     pub fn intr_exit();
+}
+
+fn default_intr_handler(intr_context: IntrContext) {
+    let intr = intr_context.intr;
+    let error_code = intr_context.error_code;
+    let eip = intr_context.eip;
+    let cs = intr_context.cs;
+    debug!("intr #{}({:#x}) error code {} {} eip {:#x} cs {:#x}", intr, intr, error_code, IrqErrorCode(error_code), eip, cs);
+    info!("no handle intr");
+    loop {}
 }
---
+++ src/intr/pic.rs
@@ -0,0 +1,78 @@
+use super::IntrContext;
+use super::IrqType;
+use super::INTR_HANDLER_TABLE;
+use crate::arch::x86::pic;
+use crate::arch::x86::outb;
+use crate::schedule::suspend_current_and_run_next;
+
+/// 主片的控制端口
+const PIC_M_CTRL: u16 = 0x20;
+/// 主片的数据端口
+const PIC_M_DATA: u16 = 0x21;
+/// 主片的控制端口
+const PIC_S_CTRL: u16 = 0xA0;
+/// 主片的数据端口
+const PIC_S_DATA: u16 = 0xA1;
+
+pub fn init() {
+    // 初始化主片
+    assert_eq!(pic::ICW1::new(true, false, false).0, 0x11u8);
+    outb(PIC_M_CTRL, pic::ICW1::new(true, false, false).0);  // ICW1: 边缘触发，级联 8259，需要ICW4
+    outb(PIC_M_DATA, pic::ICW2(0x20).0);  // ICW2: 起始中断向量为 0x20
+    assert_eq!(pic::ICW3::master(2).0, 0x04u8);
+    outb(PIC_M_DATA, pic::ICW3::master(2).0);  // ICW3: IR2 接从片
+    assert_eq!(pic::ICW4::uPM.bits(), 0x01u8);
+    outb(PIC_M_DATA, pic::ICW4::uPM.bits());  // ICW4: 8086 模式，正常 EOI
+
+    // 初始化从片
+    assert_eq!(pic::ICW1::new(true, false, false).0, 0x11u8);
+    outb(PIC_S_CTRL, pic::ICW1::new(true, false, false).0);  // ICW1: ICW1: 边缘触发，级联 8259，需要ICW4
+    outb(PIC_S_DATA, pic::ICW2(0x28).0);  // ICW2: 起始中断向量为 0x28
+    assert_eq!(pic::ICW3::slaver(2).0, 0x02u8);
+    outb(PIC_S_DATA, pic::ICW3::slaver(2).0);  // ICW3: 设置从片连接到主片的IR2引脚
+    assert_eq!(pic::ICW4::uPM.bits(), 0x01u8);
+    outb(PIC_S_DATA, pic::ICW4::uPM.bits());  // ICW4: 8086 模式，正常 EOI
+
+    // 只打开时钟中断
+    outb(PIC_M_DATA, 0xfe);
+    outb(PIC_S_DATA, 0xff);
+}
+
+fn register_pic_intr() {
+    let mut intr_handler_table = INTR_HANDLER_TABLE.lock();
+    intr_handler_table[IrqType::TIME as usize] = time_intr_handler;
+    intr_handler_table[IrqType::KEYBOARD as usize] = pic_master_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X22 as usize] = pic_master_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X23 as usize] = pic_master_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X24 as usize] = pic_master_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X25 as usize] = pic_master_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X26 as usize] = pic_master_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X27 as usize] = pic_master_intr_handler;
+
+    intr_handler_table[IrqType::IRQ_0X28 as usize] = pic_slaver_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X29 as usize] = pic_slaver_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X2A as usize] = pic_slaver_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X2B as usize] = pic_slaver_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X2C as usize] = pic_slaver_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X2D as usize] = pic_slaver_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X2E as usize] = pic_slaver_intr_handler;
+    intr_handler_table[IrqType::IRQ_0X2F as usize] = pic_slaver_intr_handler;
+}
+
+fn time_intr_handler(intr_context: IntrContext) {
+    assert_eq!(pic::OCW2::new(false, false, true, 0).0, 0x20);
+    outb(PIC_M_CTRL, pic::OCW2::new(false, false, true, 0).0);
+
+    suspend_current_and_run_next();
+}
+
+fn pic_master_intr_handler(intr_context: IntrContext) {
+    assert_eq!(pic::OCW2::new(false, false, true, 0).0, 0x20);
+    outb(PIC_M_CTRL, pic::OCW2::new(false, false, true, 0).0);
+}
+
+fn pic_slaver_intr_handler(intr_context: IntrContext) {
+    assert_eq!(pic::OCW2::new(false, false, true, 0).0, 0x20);
+    outb(PIC_S_CTRL, pic::OCW2::new(false, false, true, 0).0);
+    outb(PIC_M_CTRL, pic::OCW2::new(false, false, true, 0).0);
+}
